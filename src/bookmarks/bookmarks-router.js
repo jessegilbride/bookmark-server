40bb8a9530aeaccd8b2bbcbb0e601ff67908ccb5
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-03-22 16:25:02 -0400

    create routes

---
+++ src/bookmarks/bookmarks-router.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const { v4: uuid } = require('uuid')
+const logger = require('../logger')
+const { bookmarks } = require('../store')
+
+const bookmarksRouter = express.Router()
+const bodyParser = express.json()
+
+bookmarksRouter
+  .route('/bookmarks')
+  .get((req, res) => {
+    res.json(bookmarks);
+  })
+  .post(bodyParser, (req, res) => {
+    const { title, url, rating, desc } = req.body;
+
+    if (!title) {
+      logger.error(`title is required`);
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+    
+    if (!url) {
+      logger.error(`url is required`);
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+    
+    if (!rating) {
+      logger.error(`rating is required`);
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+    
+    if (!desc) {
+      logger.error(`description is required`);
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+    
+    // get a new id
+    const id = uuid();
+
+    const bookmark = {
+      id, 
+      title, 
+      url, 
+      rating, 
+      desc
+    };
+
+    bookmarks.push(bookmark);
+
+    logger.info(`bookmark created with id: ${id}`);
+
+    res
+      .status(201)
+      .location(`http://localhost:8000/card/${id}`)
+      .json(card);
+  })
+
+  bookmarksRouter
+  .route('/bookmarks/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    const bookmark = bookmarks.find(b => b.id === id);
+
+    if(!bookmark) {
+      res.sendStatus(404).send('Cannot find that bookmark.')
+    }
+
+    res.json(bookmark);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    const bookmarkIndex = bookmarks.findIndex(b => b.id === id);
+
+    if(bookmarkIndex === -1) {
+      res.sendStatus(404).send('No bookmark deleted, cannot find it in the list.')
+    }
+
+    bookmarks.splice(bookmarkIndex, 1);
+
+    res.sendStatus(203).send('Bookmark deleted');
+  })
+
+
+module.exports = bookmarksRouter
