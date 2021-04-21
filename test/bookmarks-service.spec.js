ad7fd7de87e1631038abde2c7e727ef355e50ed6
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-04-21 10:39:30 -0400

    create file, initial upload

---
+++ test/bookmarks-service.spec.js
@@ -0,0 +1,6 @@
+const BookmarksService = require('../src/bookmarks-service');
+const knex = require('knex');
+
+describe('Tests for BookmarksService object: database CRUD operations', function () {
+  let db;
+});
