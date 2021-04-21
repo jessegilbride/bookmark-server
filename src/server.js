380d04222ec101820a4ea3b7f00931960d88d53b
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-04-21 10:46:10 -0400

    wire up knex

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
-const app = require('./app');
-
+const app = require('./app'); // exposes server to endpoints and middleware via express
+const knex = require('knex'); // exposes server to database
 const { PORT } = require('./config');
+
+// create knex instance
+const db = knex({
+  client: 'pg',
+  connection: DB_URL,
+});
+
+app.set('db', db); // attach knex to app; avoids circular referencing between app and server files
 
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
-})
-
+});
