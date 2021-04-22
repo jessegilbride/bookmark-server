23b5546f7e731ab0bd09f3b330d1b33a2fbe853d
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-04-22 18:22:26 -0400

    comment-out bearer token validation

--- src/app.js
+++ src/app.js
@@ -5,42 +5,42 @@ const helmet = require('helmet');
 const logger = require('./logger');
 const { NODE_ENV } = require('./config');
 require('dotenv').config();
-const bookmarksRouter = require('./bookmarks/bookmarks-router')
+const bookmarksRouter = require('./bookmarks/bookmarks-router');
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
+// app.use(function validateBearerToken(req, res, next) {
+//   const apiToken = process.env.API_TOKEN
+//   const authToken = req.get('Authorization')
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: 'Unauthorized request' })
-  }
-  
-  next()
-})
+//   if (!authToken || authToken.split(' ')[1] !== apiToken) {
+//     logger.error(`Unauthorized request to path: ${req.path}`);
+//     return res.status(401).json({ error: 'Unauthorized request' })
+//   }
+
+//   next()
+// })
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error'} }
+    response = { error: { message: 'server error' } };
   } else {
     console.error(error);
-    response = { message: error.message, error}
+    response = { message: error.message, error };
   }
   res.status(500).json(response);
 });
 
 app.use(bookmarksRouter);
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send('Hello, world!');
-})
+});
 
 module.exports = app;
