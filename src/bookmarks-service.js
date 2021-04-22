1ea8be10024640c26e8b40a971edcca3648f8907
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-04-22 18:23:30 -0400

    reordered methods, made names more semantic

--- src/bookmarks-service.js
+++ src/bookmarks-service.js
@@ -2,6 +2,9 @@ const BookmarksService = {
   getAllBookmarks(knex) {
     return knex.select('*').from('bookmarks');
   },
+  getBookmarkById(knex, id) {
+    return knex.from('bookmarks').select('*').where('id', id).first();
+  },
   insertBookmark(knex, newBookmark) {
     return knex
       .insert(newBookmark)
@@ -11,14 +14,11 @@ const BookmarksService = {
         return rows[0];
       });
   },
-  getById(knex, id) {
-    return knex.from('bookmarks').select('*').where('id', id).first();
-  },
-  deleteArticle(knex, id) {
-    return knex('bookmarks').where({ id }).delete();
-  },
-  updateArticle(knex, id, newBookmarkFields) {
+  updateBookmark(knex, id, newBookmarkFields) {
     return knex('bookmarks').where({ id }).update(newBookmarkFields);
+  },
+  deleteBookmark(knex, id) {
+    return knex('bookmarks').where({ id }).delete();
   },
 };
 
