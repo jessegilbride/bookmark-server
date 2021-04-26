512b754fc7e4e6981c56cd183ff0113423b5bc72
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-04-26 13:52:02 -0400

    change get all and get by id to use database

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const BookmarksService = require('../bookmarks-service');
 const { v4: uuid } = require('uuid');
 const logger = require('../logger');
-// const { bookmarks } = require('../store');
+// const { bookmarks: storeBookmarks } = require('../store');
+const store = require('../store');
 
 const bookmarksRouter = express.Router();
 const bodyParser = express.json();
 
 bookmarksRouter
   .route('/bookmarks')
-  .get((req, res) => {
-    res.json(bookmarks);
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db'); // gets app.db, which is the knex instance created in server.js
+    BookmarksService.getAllBookmarks(knexInstance)
+      .then((bookmarks) => {
+        res.json(bookmarks);
+      })
+      .catch(next);
   })
   .post(bodyParser, (req, res) => {
     const { title, url, rating, description } = req.body;
@@ -42,7 +49,9 @@ bookmarksRouter
       description,
     };
 
-    bookmarks.push(bookmark);
+    store.bookmarks.push(bookmark); // <<<<<< CHANGE <<<<<<
+    // const knexInstance = req.app.get('db');
+    // BookmarksService.insertBookmark(db, bookmark); // not sure if this is correct, I attempted it before the checkpoint says to
 
     logger.info(`bookmark created with id: ${id}`);
 
@@ -54,25 +63,29 @@ bookmarksRouter
 
 bookmarksRouter
   .route('/bookmarks/:id')
-  .get((req, res) => {
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db');
     const { id } = req.params;
-    const bookmark = bookmarks.find((b) => b.id === id);
-
-    if (!bookmark) {
-      res.status(404).send('Cannot find that bookmark.');
-    }
-
-    res.json(bookmark);
+    // const bookmark = bookmarks.find((bkmrk) => bkmrk.id === id); // <<<<<< CHANGE <<<<<<
+    BookmarksService.getBookmarkById(knexInstance, id)
+      .then((bookmark) => {
+        if (!bookmark) {
+          logger.error(`Bookmark "${id}" not found.`);
+          res.status(404).send(`Cannot find that bookmark.`);
+        }
+        res.json(bookmark);
+      })
+      .catch(next);
   })
   .delete((req, res) => {
     const { id } = req.params;
-    const bookmarkIndex = bookmarks.findIndex((b) => b.id == id);
+    const bookmarkIndex = store.bookmarks.findIndex((b) => b.id == id); // <<<<<< CHANGE <<<<<<
 
     if (bookmarkIndex === -1) {
       res.status(404).send('No bookmark deleted, cannot find it in the list.');
     }
 
-    bookmarks.splice(bookmarkIndex, 1);
+    store.bookmarks.splice(bookmarkIndex, 1); // <<<<<< CHANGE <<<<<<
 
     res.status(204).end();
   });
