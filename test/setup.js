64b9bfcaf8a19648662ec448edc730dc93dc2d2c
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-04-26 13:54:29 -0400

    remove dotenv package import, add knex connection

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,15 @@
-require('dotenv').config();
 const { expect } = require('chai');
+const knex = require('knex');
 const supertest = require('supertest');
+
+const { TEST_DB_URL } = require('../src/config');
+
+db = knex({
+  client: 'pg',
+  connection: TEST_DB_URL,
+});
+
+global.db = db;
 
 global.expect = expect;
 global.supertest = supertest;
