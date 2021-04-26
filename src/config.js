c2fbe0bfd055cc6432f3888f9d59a9343a1b4635
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-04-26 13:59:39 -0400

    add TEST_DB_URL

--- src/config.js
+++ src/config.js
@@ -3,4 +3,8 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL:
     process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
+  TEST_DB_URL:
+    process.env.TEST_DB_URL ||
+    'postgresql://dunder_mifflin@localhost/bookmarks-test',
+  API_TOKEN: process.env.API_TOKEN,
 };
