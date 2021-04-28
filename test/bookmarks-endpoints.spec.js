225caffd5911625828c03ef306668a32c5531ad8
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-04-28 18:26:54 -0400

    add POST and DELETE endpoint tests

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -1,11 +1,10 @@
 const app = require('../src/app'); // the module to test
-const makeTestBookmarks = require('./bookmarks.fixtures');
-
-const testBookmarks = makeTestBookmarks();
+const { v4: uuid } = require('uuid'); // for bookmarkId variable in test to delete a bookmark when no bookmarks exist
+const { makeTestBookmarks } = require('./bookmarks.fixtures');
 
 describe('Testing bookmarks endpoints...', function () {
   before('make knex instance', () => {
-    app.set('db', db); // necessary?
+    app.set('db', db); // necessary
   });
 
   before('clean the table (before all tests)', () =>
@@ -18,8 +17,8 @@ describe('Testing bookmarks endpoints...', function () {
   after('disconnect from db', () => db.destroy());
 
   describe('GET /bookmarks', function () {
-    context('Given there are no bookmarks:', () => {
-      it.only(`responds with 200 and an empty list`, (done) => {
+    context('Given there are *NO* bookmarks:', () => {
+      it(`responds with 200 and an empty list`, (done) => {
         supertest(app).get('/bookmarks').expect(200, []);
         done();
         return;
@@ -27,7 +26,7 @@ describe('Testing bookmarks endpoints...', function () {
     });
 
     context('Given there are bookmarks in the database:', () => {
-      // const testBookmarks = makeTestBookmarks(); // pulling this out, put into global scope of file to be shared between tests and to be exported to bookmarks-router (for endpoint testing)
+      const testBookmarks = makeTestBookmarks(); // pulling this out, put into global scope of file to be shared between tests and to be exported to bookmarks-router (for endpoint testing)
 
       beforeEach('insert bookmarks', () => {
         return db.into('bookmarks').insert(testBookmarks);
@@ -38,24 +37,64 @@ describe('Testing bookmarks endpoints...', function () {
         done();
         return;
       });
+    });
+  });
 
-      /* sanity test */
-      // it('should not fail', function (done) {
-      //   try {
-      //     supertest(app).get('/bookmarks').expect(200, testBookmarks);
-      //     done();
-      //   } catch (error) {
-      //     done(error);
-      //   }
-      // });
+  describe(`POST /bookmarks`, () => {
+    it(`creates an bookmark, responding with 201 and the new bookmark`, function () {
+      const newBookmark = {
+        title: 'New Test Bookmark',
+        url: 'https://www.thisIsA.test',
+        rating: '4',
+        description: 'New bookmark description'
+      };
+      return supertest(app)
+        .post('/bookmarks')
+        .send(newBookmark)
+        .expect(201)
+        .expect((res) => {
+          expect(res.body.title).to.eql(newBookmark.title);
+          expect(res.body.url).to.eql(newBookmark.url);
+          expect(res.body.rating).to.eql(newBookmark.rating);
+          expect(res.body.description).to.eql(newBookmark.description);
+          expect(res.body).to.have.property('id');
+          expect(res.headers.location).to.eql(`/bookmarks/${res.body.id}`);
+        })
+        .then((postRes) =>
+          supertest(app)
+            .get(`/bookmarks/${postRes.body.id}`)
+            .expect(postRes.body)
+        );
+    });
+
+    const requiredFields = ['title', 'url', 'rating', 'description'];
+
+    requiredFields.forEach((field) => {
+      const newBookmark = {
+        title: 'New Test Bookmark',
+        url: 'https://www.thisIsA.test',
+        rating: '4',
+        description: 'New bookmark description'
+      };
+
+      it(`responds with 400 and error message when the '${field}' is missing`, () => {
+        delete newBookmark[field];
+
+        return supertest(app)
+          .post('/bookmarks')
+          .send(newBookmark)
+          .expect(400, {
+            error: { message: `Missing '${field}' in request body` }
+          });
+      });
     });
   });
 
   describe('GET /bookmarks/:id', function () {
-    context('Given there are no bookmarks:', () => {});
+    context('Given there are *NO* bookmarks in the database:', () => {});
 
     context('Given there are bookmarks in the database:', () => {
-      // const testBookmarks = makeTestBookmarks(); // pulling this out, put into global scope of file to be shared between tests and to be exported to bookmarks-router (for endpoint testing)
+      const testBookmarks = makeTestBookmarks(); // pulling this out, put into global scope of file to be shared between tests and to be exported to bookmarks-router (for endpoint testing)
 
       beforeEach('insert bookmarks', () => {
         return db.into('bookmarks').insert(testBookmarks);
@@ -75,8 +114,36 @@ describe('Testing bookmarks endpoints...', function () {
       });
     });
   });
-});
 
-module.exports = {
-  testBookmarks,
-};
+  describe(`DELETE /bookmarks/:id`, () => {
+    context('Given there are bookmarks in the database', () => {
+      const testBookmarks = makeTestBookmarks();
+
+      beforeEach('insert bookmarks', () => {
+        return db.into('bookmarks').insert(testBookmarks);
+      });
+
+      it('responds with 204 and removes the bookmark', () => {
+        const idToRemove = testBookmarks[0].id;
+        const expectedBookmarks = testBookmarks.filter(
+          (bookmark) => bookmark.id !== idToRemove
+        );
+        return supertest(app)
+          .delete(`/bookmarks/${idToRemove}`)
+          .expect(204)
+          .then((res) =>
+            supertest(app).get(`/bookmarks`).expect(expectedBookmarks)
+          );
+      });
+    });
+
+    context(`Given there are *NO* bookmarks in the database`, () => {
+      it(`responds with 404`, () => {
+        const bookmarkId = uuid(); // expected to be type: UUID
+        return supertest(app)
+          .delete(`/bookmarks/${bookmarkId}`)
+          .expect(404, { error: { message: `Bookmark doesn't exist` } });
+      });
+    });
+  });
+});
